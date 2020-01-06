904df2522429f2136ec597ef1b0f74b22c76a5fe
Thomas Nguyen <TNguyen@spartaglobal.com>
2020-01-06 12:05:43 +0000

    Provisioned Ansible and created a playbook which installs the required files

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,11 @@
 Vagrant.configure("2") do |config|
-
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "ubuntu/xenial64" # Methods for properties etc
   config.vm.network "private_network", ip: "192.168.10.100"
   config.hostsupdater.aliases = ["development.local"]
 
+  # Run Ansible from the Vagrant Host
+  config.vm.provision "ansible" do |ansible|
+    ansible.verbose = "v"
+    ansible.playbook = "playbook.yml"
+  end
 end
---
+++ environment/provision.sh
@@ -0,0 +1,24 @@
+# sudo apt update
+# sudo apt install software-properties-common
+# sudo apt-add-repository --yes --update ppa:ansible/ansible
+# sudo apt install ansible
+#
+# # Update the sources list
+# sudo apt-get update -y
+#
+# # upgrade any packages available
+# sudo apt-get upgrade -y
+#
+# # install nginx
+# sudo apt-get install nginx -y
+#
+# # install git
+# sudo apt-get install git -y
+#
+# # install nodejs
+# sudo apt-get install python-software-properties
+# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# sudo apt-get install nodejs -y
+#
+# # install pm2
+# sudo npm install pm2 -g
